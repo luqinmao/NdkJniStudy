68ab99df6c1339f0f68a918713d1cf1cc59f23b6
luqinmao <1002438809@qq.com>
2018-01-17 14:26:43 +0800

    log 打印成功

--- app/src/main/cpp/Hello.cpp
+++ app/src/main/cpp/Hello.cpp
@@ -3,7 +3,15 @@
 //C++ 具体实现类
 
 #include "com_lqm_ndkjnistudy_Hello.h"
+
+#include <android/log.h>
+#define TAG    "nate"
+#define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,TAG,__VA_ARGS__)
+
 JNIEXPORT jstring JNICALL Java_com_lqm_ndkjnistudy_Hello_sayHello
         (JNIEnv * env, jclass jclass1){
+
+    LOGD("from c log");
+
     return env ->NewStringUTF("from c");
 }
--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,6 +1,10 @@
 #include <jni.h>
 #include <string>
 
+#include <android/log.h>
+#define TAG    "nate"
+#define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,TAG,__VA_ARGS__)
+
 extern "C"
 JNIEXPORT jstring
 
@@ -8,6 +12,9 @@ JNICALL
 Java_com_lqm_ndkjnistudy_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
+
+    LOGD("from native-lib log");
+
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
